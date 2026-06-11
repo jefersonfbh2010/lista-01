f5befb4ad87a5ea1cf37618b66f8b2760b0f7e27
Jeferson <jefersonfbh2010@gmail.com>
2026-06-11 20:08:55 -0300

    questão 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Informe a idade do visitante: ");
+
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+
+if (variavelQueSalvaUmNumero >= 16)
+{
+    Console.WriteLine("Entrada liberada.");
+}
+else
+{
+    Console.WriteLine("Entrada permitida somente com responsável.");
+}
+
+Console.WriteLine ("solicitar nota do aluno")
